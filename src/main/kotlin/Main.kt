8e284c911d30cfc280b2027a4e8003ea4f0e7b24
Anastasia Iozefi <aiozefi@gmail.com>
2024-01-23 01:36:08 +0300

    Change Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val purchaseAmount = 11000.0 // Замените это значение на актуальную сумму покупки
+    val purchaseAmount = 5000.0 // Замените это значение на актуальную сумму покупки
     val isRegularCustomer = true // Замените на true, если пользователь является постоянным, иначе false
 
     val discount = calculateDiscount(purchaseAmount, isRegularCustomer)
@@ -11,12 +11,11 @@ fun main() {
 }
 
 fun calculateDiscount(amount: Double, isRegularCustomer: Boolean): Double {
-    val baseDiscount = if (amount >= 10001.0) amount * 0.05 else 0.0
+    val baseDiscount = if (amount >= 10001.0) amount * 0.05 else if (amount >= 1001.0) 100.0 else 0.0
     val regularCustomerDiscount = if (isRegularCustomer) (amount - baseDiscount) * 0.01 else 0.0
 
     return when {
         amount in 0.0..1000.0 -> regularCustomerDiscount
-        amount in 1001.0..10000.0 -> 100.0 + regularCustomerDiscount
         else -> baseDiscount + regularCustomerDiscount
     }
 }
